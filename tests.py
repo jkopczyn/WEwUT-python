44df3712f0a22026a65f2deeb8e9c5e83b0a7ec5
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-04-25 11:23:42 -0700

    initial commit for test file

---
+++ tests.py
@@ -0,0 +1,15 @@
+from builder import CustomerBuilder
+from builder import RentalBuilder
+from builder import MovieBuilder
+from customer import Customer
+from movie import Movie
+from movie import MOVIE_TYPES
+from movie import TYPE_NEW_RELEASE
+from movie import TYPE_REGULAR
+from movie import TYPE_CHILDREN
+from movie import TYPE_UNKNOWN
+from rental import Rental
+from unittest import TestCase
+
+if __name__ == '__main__':
+    unittest.main()
