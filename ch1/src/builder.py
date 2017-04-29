3791b2212f72b9b5b549975c4a0ef851493b0882
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-04-28 23:21:20 -0700

    misread chapter boundaries, combined files

--- ch1/src/builder.py
+++ ch1/src/builder.py
@@ -1,11 +1,12 @@
 from customer import Customer
-from rental import Rental
 from movie import Movie
 from movie import MOVIE_TYPES
 from movie import TYPE_NEW_RELEASE
 from movie import TYPE_REGULAR
 from movie import TYPE_CHILDREN
 from movie import TYPE_UNKNOWN
+from rental import Rental
+from store  import Store
 
 class CustomerBuilder(object):
     def __init__(self, name="Jim", rentals=None, builders=None, builder=None):
@@ -25,8 +26,13 @@ class CustomerBuilder(object):
             result.add_rental(rental)
         return result
 
-a = CustomerBuilder()
-b = a.build()
+class MovieBuilder(object):
+    def __init__(self, name="Godfather 4", movietype=TYPE_NEW_RELEASE):
+        self.name = name
+        self.type = movietype
+
+    def build(self):
+        return Movie(self.name, self.type)
 
 class RentalBuilder(object):
     def __init__(self, movie=None, days=3, builder=None):
@@ -38,16 +44,14 @@ class RentalBuilder(object):
     def build(self):
         return Rental(self.movie, self.days)
 
-c = RentalBuilder()
-d = c.build()
-
-class MovieBuilder(object):
-    def __init__(self, name="Godfather 4", movietype=TYPE_NEW_RELEASE):
-        self.name = name
-        self.type = movietype
+class StoreBuilder(object):
+    def __init__(self, movies=[], movie=None, builders=[], builder=None):
+        self.movies = movies
+        if movie:
+            self.movies += [movie]
+        self.builders = builders
+        if builder:
+            self.builders += [builder]
 
     def build(self):
-        return Movie(self.name, self.type)
-
-e = MovieBuilder()
-f = e.build()
+        return Store(self.movies + [b.build() for b in self.builders])
--- ch1/src/rental.py
+++ ch1/src/rental.py
@@ -2,6 +2,7 @@ class Rental(object):
     def __init__(self, movie, days):
         self.movie = movie
         self.days = days
+        self.started = False
 
     def get_charge(self):
         return self.movie.get_charge(self.days)
@@ -11,3 +12,12 @@ class Rental(object):
 
     def get_line_item(self):
         return "{0} {1}".format(self.movie.get_title(), self.get_charge())
+
+    def start(self, store):
+        if store.get_availability(self.movie):
+            store.check_out(self.movie)
+            self.started = True
+        return self.started
+
+    def is_started(self):
+        return self.started
