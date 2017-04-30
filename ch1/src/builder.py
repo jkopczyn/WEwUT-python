5ceb2601602c70ed640fcab5ec477aa258320c73
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-04-29 22:11:37 -0700

    Revert "misread chapter boundaries, combined files"
    This reverts commit 162178341b304e0b56608015846314949a5e5ccd.
    It turns out I'd read it right the first time.

--- ch1/src/builder.py
+++ ch1/src/builder.py
@@ -1,12 +1,11 @@
 from customer import Customer
+from rental import Rental
 from movie import Movie
 from movie import MOVIE_TYPES
 from movie import TYPE_NEW_RELEASE
 from movie import TYPE_REGULAR
 from movie import TYPE_CHILDREN
 from movie import TYPE_UNKNOWN
-from rental import Rental
-from store  import Store
 
 class CustomerBuilder(object):
     def __init__(self, name="Jim", rentals=None, builders=None, builder=None):
@@ -26,13 +25,8 @@ class CustomerBuilder(object):
             result.add_rental(rental)
         return result
 
-class MovieBuilder(object):
-    def __init__(self, name="Godfather 4", movietype=TYPE_NEW_RELEASE):
-        self.name = name
-        self.type = movietype
-
-    def build(self):
-        return Movie(self.name, self.type)
+a = CustomerBuilder()
+b = a.build()
 
 class RentalBuilder(object):
     def __init__(self, movie=None, days=3, builder=None):
@@ -44,14 +38,16 @@ class RentalBuilder(object):
     def build(self):
         return Rental(self.movie, self.days)
 
-class StoreBuilder(object):
-    def __init__(self, movies=[], movie=None, builders=[], builder=None):
-        self.movies = movies
-        if movie:
-            self.movies += [movie]
-        self.builders = builders
-        if builder:
-            self.builders += [builder]
+c = RentalBuilder()
+d = c.build()
+
+class MovieBuilder(object):
+    def __init__(self, name="Godfather 4", movietype=TYPE_NEW_RELEASE):
+        self.name = name
+        self.type = movietype
 
     def build(self):
-        return Store(self.movies + [b.build() for b in self.builders])
+        return Movie(self.name, self.type)
+
+e = MovieBuilder()
+f = e.build()
