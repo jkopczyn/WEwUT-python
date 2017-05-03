8bc2948134e96b96feaa01e2699260ac366ea4b5
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-05-03 12:46:07 -0700

    code under test for too-specific tests

---
+++ ch4_overspecific/src/customer.py
@@ -0,0 +1,25 @@
+class Customer(object):
+    def __init__(self, name):
+        self.name = name
+        self.rentals = []
+
+    def add_rental(self, rental):
+        self.rentals.append(rental)
+
+    def get_rentals(self):
+        return self.rentals
+
+    def recent_rentals(self):
+        prefix = "Recent rentals:"
+        return (prefix + "\n".join("", *[r.get_movie(
+            true
+            ).get_title(
+                "{0} starring {1} {2}", 2
+                ) for r in rentals[:3]]))
+
+
+    def get_total_charge(self):
+        return sum(r.get_charge() for r in self.rentals)
+
+    def get_total_points(self):
+        return sum(r.get_points() for r in self.rentals)
