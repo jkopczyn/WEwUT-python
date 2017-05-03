3317dfe25835db028798e1eb2bfab471a8541fc2
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-05-02 17:16:08 -0700

    split into single-assert tests

--- ch4/tests/tests_with_single_asserts.py
+++ ch4/tests/tests_with_single_asserts.py
@@ -10,62 +10,80 @@ from unittest import TestCase
 import unittest
 
 class TestMovie(TestCase):
-    def test_charge_for_children_movies(self):
+    def test_charge_for_children_1_day(self):
         self.assertEqual(1.5,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_charge(1))
+    def test_charge_for_children_2_day(self):
         self.assertEqual(1.5,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_charge(2))
+    def test_charge_for_children_3_day(self):
         self.assertEqual(1.5,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_charge(3))
+    def test_charge_for_children_4_day(self):
         self.assertEqual(3.0,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_charge(4))
+    def test_charge_for_children_5_day(self):
         self.assertEqual(4.5,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_charge(5))
 
-    def test_charge_for_new_release_movies(self):
+    def test_charge_for_new_release_1_day(self):
         self.assertEqual(3.0,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_charge(1))
+    def test_charge_for_new_release_2_day(self):
         self.assertEqual(6.0,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_charge(2))
+    def test_charge_for_new_release_3_day(self):
         self.assertEqual(9.0,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_charge(3))
+    def test_charge_for_new_release_4_day(self):
         self.assertEqual(12.0,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_charge(4))
 
-    def test_charge_for_regular_movies(self):
+    def test_charge_for_regular_1_day(self):
         self.assertEqual(2.0,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_charge(1))
+    def test_charge_for_regular_2_day(self):
         self.assertEqual(2.0,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_charge(2))
+    def test_charge_for_regular_3_day(self):
         self.assertEqual(3.5,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_charge(3))
+    def test_charge_for_regular_4_day(self):
         self.assertEqual(5.0,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_charge(4))
+    def test_charge_for_regular_5_day(self):
         self.assertEqual(6.5,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_charge(5))
 
-    def test_points_for_children_movies(self):
+    def test_points_for_children_1_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_points(1))
+    def test_points_for_children_2_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_points(2))
+    def test_points_for_children_3_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_CHILDREN).build().get_points(3))
 
-    def test_points_for_new_release_movies(self):
+    def test_points_for_new_release_1_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_points(1))
+    def test_points_for_new_release_2_day(self):
         self.assertEqual(2,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_points(2))
+    def test_points_for_new_release_3_day(self):
         self.assertEqual(2,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_points(3))
+    def test_points_for_new_release_4_day(self):
         self.assertEqual(2,
                 MovieBuilder(movietype=TYPE_NEW_RELEASE).build().get_points(4))
 
-    def test_points_for_regular_movies(self):
+    def test_points_for_regular_1_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_points(1))
+    def test_points_for_regular_2_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_points(2))
+    def test_points_for_regular_3_day(self):
         self.assertEqual(1,
                 MovieBuilder(movietype=TYPE_REGULAR).build().get_points(3))
